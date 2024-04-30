f38af7fbc056c06df80c8d060c0d29264ca05084
JRay <lin299579@gmail.com>
2024-05-01 00:36:00 +0800

    1.2.0

--- client/src/components/Home/content/Links.js
+++ client/src/components/Home/content/Links.js
@@ -2,23 +2,23 @@ import React, { useState } from "react";
 import { Box, Button } from "@mui/material";
 
 export default function Links({ userInfo }) {
-  const websiteMap = userInfo.settings.link;
+    const websiteMap = userInfo.link;
 
-  return (
-    <Box sx={{ mx: 4, my: 2 }}>
-      {Object.entries(websiteMap).map(([name, url]) => (
-        <Button
-          key={name}
-          variant="contained"
-          color="primary"
-          href={url}
-          target="_blank"
-          rel="noopener noreferrer"
-          sx={{ mx: 2, my: 1 }}
-        >
-          {name}
-        </Button>
-      ))}
-    </Box>
-  );
+    return (
+        <Box sx={{ mx: 4, my: 2 }}>
+            {Object.entries(websiteMap).map(([name, url]) => (
+                <Button
+                    key={name}
+                    variant="contained"
+                    color="primary"
+                    href={url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    sx={{ mx: 2, my: 1 }}
+                >
+                    {name}
+                </Button>
+            ))}
+        </Box>
+    );
 }
